c1902b5a608e146fe55ddf8de240ab24bfdd7db6
mercedesguerrero <m.mercedesguerrero@gmail.com>
2016-11-22 19:59:03 -0300

    Clase4

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -16,13 +16,74 @@ function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
 	 
-	
+	contadorIntentos=0;	
+
+	numeroSecreto=Math.floor((Math.random() * 100) + 1);
+
+	alert(+numeroSecreto);
 
 }
 
 function verificar()
 {
-	
+	var numero;
+	numero= document.getElementById('numero').value;
+
+	//hay 3 maneras...
+	//contadorIntentos= contadorIntentos+1;
+	//contadorIntentos+=1;
+	contadorIntentos++;
+	document.getElementById('intentos').value= contadorIntentos;
+
+	if (numero== numeroSecreto) 
+	{
+	alert("Ganaste!");
+		if (contadorIntentos==1)
+		{
+		alert("Usted es un psíquico");
+		}
+		if (contadorIntentos==2)
+		{
+		alert("Excelente percepción");
+		}
+		if (contadorIntentos==3)
+		{
+		alert("Esto es suerte");
+		}
+		if (contadorIntentos==4)
+		{
+		alert("Excelente técnica");
+		}
+		if (contadorIntentos==5)
+		{
+		alert("Usted está en la media");
+		}
+		if (contadorIntentos>5 && contadorIntentos<11)
+		{
+		alert("Falta técnica")
+		}
+		if (contadorIntentos>=11)
+		{
+		alert("Afortunado en el amor <3 !!")
+		}
+
+	}
+		else
+		{
+
+			if (numero< numeroSecreto)
+			{
+			alert("Falta...")
+
+			}
+
+			else	
+			{
+			alert("Se pasó...")
+
+			}
+		}
+
 	
 
 }
