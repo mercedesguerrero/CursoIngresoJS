7b6b29dd80983fc0aa7d001064bc55af4670ce41
mercedesguerrero <m.mercedesguerrero@gmail.com>
2016-12-06 19:57:33 -0300

    Clase7

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,80 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+ 	var edad;
+ 	var sexo;
+ 	var estadoCivil;
+ 	var SueldoBruto;
+ 	var legajo;
+
+ 	edad=prompt("Ingrese edad entre 18 y 90");
+
+ 	while(edad>90 || edad<18)
+ 	{
+ 		edad=prompt("Re-ingrese edad entre 18 y 90");
+ 		edad=parseInt(edad);
+ 	}
+ 	//la edad está correcta
+ 	document.getElementById('Edad').value=edad;
+
+ 	sexo=prompt("Ingrese f ó m");
+
+ 	while(sexo!="f" && sexo!="m")
+ 	{
+ 		sexo=prompt("Reingrese f ó m");
+ 	}
+ 	//el sexo está correcto
+ 	document.getElementById('Sexo').value=sexo;
+
+ 	estadoCivil=prompt("Ingrese estado civil, 1-para soltero, 2-para casado, 3-para divorciado y 4-para viudo");
+
+ 	while(estadoCivil!="1" && estadoCivil!="2" && estadoCivil!="3" && estadoCivil!="4")
+ 	// si ponemos (estadoCivil<1 || estadoCivil>4) hay que poner abajo del prompt:
+ 	// estadoCivil=parseInt(EstadoCivil);
+ 	{
+ 		estadoCivil=prompt("Re-ingrese estado civil, 1-para soltero, 2-para casado, 3-para divorciado y 4-para viudo");
+ 	}
+ 	//el estado civil está correcto
+ 		switch(estadoCivil)
+ 			{
+ 				case "1":
+ 					document.getElementById('EstadoCivil').value="Soltero";
+ 				break;
+ 				case "2":
+ 					document.getElementById('EstadoCivil').value="Casado";
+ 				break;
+ 				case "3":
+ 					document.getElementById('EstadoCivil').value="Divorciado";
+ 				break;
+ 				case "4":
+ 					document.getElementById('EstadoCivil').value="Viudo";
+ 				break;
+ 			}
+ 		//* Otra manera:
+ 		// switch(estadoCivil)
+ 		// case "1":
+ 		//	estadoCivil="soltero";
+ 		// break;
+
+ 	SueldoBruto=prompt("Ingrese sueldo bruto");
+ 	SueldoBruto=parseInt(SueldoBruto);
+
+ 	while(SueldoBruto<8000)
+ 	{
+ 		SueldoBruto=prompt("Re-ingrese sueldo bruto");
+ 		SueldoBruto=parseInt(SueldoBruto);
+ 	}
+ 	document.getElementById('Sueldo').value=SueldoBruto;
+
+ 	legajo=prompt("Ingrese legajo, numérico de 4 cifras, sin ceros a la izquierda")
+
+ 	while(legajo<10000 && legajo>999)
+ 	{
+ 		legajo=
+ 	}
+
+
+
+
+
 }
